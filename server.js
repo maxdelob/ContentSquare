844789dd81a528a58bf8776fb6a11aaf8fb18fd2
maxdelob <maxime.delobelle@gmail.com>
2019-09-20 01:00:40 +0200

    heroku - commit error

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/contentsquare'));
+app.use(express.static(__dirname + '/dist/ContentSquare'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/contentsquare/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ContentSquare/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
